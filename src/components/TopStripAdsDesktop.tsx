519e1761fa054e5bb420836162ad5b5e5448c8e4
Anthony PC <simplyanthony007@gmail.com>
2023-11-12 13:39:36 +0100

    Added all necessary dependencies and started the project officially

---
+++ src/components/TopStripAdsDesktop.tsx
@@ -0,0 +1,11 @@
+import TopStripGif from "../assets/Top-strip-Ads-Desktop.gif";
+
+function TopStripAdsDesktop() {
+  return (
+    <div id="top-strip-ads-container">
+      <img className="m-auto" src={TopStripGif} alt="Top Strip ADS" width="95%" height="auto" />
+    </div>
+  );
+}
+
+export default TopStripAdsDesktop;
